0a60d9ed62a9eead047a390d875467c7258c0952
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-03-09 20:26:40 +0200

    adding the transaction page

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+const tranactions = [];
+
+const add = document.getElementById("add-transaction");
+
+const tranactionType = document.getElementById("transaction-type").value;
+
+
+add.addEventListener("click",function () {
+
+  console.log(tranactionType);
+  
+});
