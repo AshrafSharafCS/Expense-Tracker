b0caab61d4c00ef981b983149f292d6aeec199db
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-03-10 13:11:05 +0200

    fetching the API that convert amounts from and to a currency

--- scripts/main.js
+++ scripts/main.js
@@ -1,56 +1,84 @@
 const tranactions = [];
 
-const add = document.getElementById("add-transaction");
-
+const addTrans = document.getElementById("add-transaction");
+const currency = document.getElementById("currency").value;
 const tranactionType = document.getElementById("transaction-type").value;
 
-// add.addEventListener("click", function () {
-//   let amount = document.getElementById("amount").value;
-// });
+addTrans.addEventListener("click", function () {
+  let amount = document.getElementById("amount").value;
+  console.log(currency);
 
+  // alert(tranactionType+"  "+amount+"  "+currency);
+});
 
 // fetching the api to get the currencies.
-// result = fetch("https://ivory-ostrich-yoke.cyclic.app/students/available", {
-//   method: "GET",
-// });
-// result
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data);
-//     for(let i=0;i<data.length;i++){
-//       addOption(data[i].symbol);
-//     }
-//   });
+result = fetch("https://ivory-ostrich-yoke.cyclic.app/students/available", {
+  method: "GET",
+});
+result
+  .then((response) => response.json())
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      addOption(data[i].code, data[i].code);
+    }
+  });
 
 // function to create an option from the results of the API
-  // function addOption(value){
-  //   let select=document.getElementById("currency");
-  //   let option=document.createElement("option");
-  //   option.value= value;
-  //   option.text=value;
-  //   select.appendChild(option);
+function addOption(value, code) {
+  let select = document.getElementById("currency");
+  let option = document.createElement("option");
+  option.value = value;
+  option.text = code;
+  select.appendChild(option);
+}
 
-  // }
-
-
-// let userData ;
+// let userData = new FormData();
 
 // userData.append("from", "EUR");
 // userData.append("to", "USD");
-// userData.append("amount", "1000");
+// userData.append("amount", 1000);
 
-
-// ash={
-//   form: 'EUR',
-//   to: 'USD',
-//   amount:100
-// };
-
-// const created = fetch("https://ivory-ostrich-yoke.cyclic.app/students/convert", {
-//   method: "POST",
-//   body: JSON.stringify(ash)
+// let ashraf=JSON.stringify({
+//   "from": "EUR",
+//   "to": "USD",
+//   "amount": 232
 // });
 
-// created.then(response=>response.json())
-// .then((data)=>{
-//   console.log("wowww!!!");});
+// const created = fetch(
+//   "https://ivory-ostrich-yoke.cyclic.app/students/convert",
+//   {
+//     method: "POST",
+//     mode: "no-cors",
+//     body: ashraf
+//   }
+// );
+
+// created
+//   .then((response) => {
+//     return response;
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+function Convert(){
+const dataa = JSON.stringify({
+  from: "EUR",
+  to: "USD",
+  amount: 1000,
+});
+
+const request = {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: dataa,
+};
+
+fetch("https://ivory-ostrich-yoke.cyclic.app/students/convert", request)
+  .then((response) => {
+    return response.text();
+  })
+  .then((result) => {
+    return result;
+  });
+}
