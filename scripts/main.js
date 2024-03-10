46259754b9ea354d9c332e010fdfb5eb85b6d6d4
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-03-10 02:22:39 +0200

    fetching the api for the currencies

--- scripts/main.js
+++ scripts/main.js
@@ -4,13 +4,53 @@ const add = document.getElementById("add-transaction");
 
 const tranactionType = document.getElementById("transaction-type").value;
 
-add.addEventListener("click", function () {
-  let amount = document.getElementById("amount").value;
-
-});
-
-window.onload=alert("hello");
+// add.addEventListener("click", function () {
+//   let amount = document.getElementById("amount").value;
+// });
 
 
+// fetching the api to get the currencies.
+// result = fetch("https://ivory-ostrich-yoke.cyclic.app/students/available", {
+//   method: "GET",
+// });
+// result
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     for(let i=0;i<data.length;i++){
+//       addOption(data[i].symbol);
+//     }
+//   });
+
+// function to create an option from the results of the API
+  // function addOption(value){
+  //   let select=document.getElementById("currency");
+  //   let option=document.createElement("option");
+  //   option.value= value;
+  //   option.text=value;
+  //   select.appendChild(option);
+
+  // }
 
 
+// let userData ;
+
+// userData.append("from", "EUR");
+// userData.append("to", "USD");
+// userData.append("amount", "1000");
+
+
+// ash={
+//   form: 'EUR',
+//   to: 'USD',
+//   amount:100
+// };
+
+// const created = fetch("https://ivory-ostrich-yoke.cyclic.app/students/convert", {
+//   method: "POST",
+//   body: JSON.stringify(ash)
+// });
+
+// created.then(response=>response.json())
+// .then((data)=>{
+//   console.log("wowww!!!");});
